72c3dd970648494271b40d039eeff5581e19fae9
JefferyWa <sevafara@gmail.com>
2018-04-10 21:13:49 +0300

    Init - cmake .. -G"Unix Makefiles"

---
+++ 2/cmake-build/CMakeFiles/FileDB.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FileDB.dir/main.cpp.o"
+  "CMakeFiles/FileDB.dir/files.cpp.o"
+  "CMakeFiles/FileDB.dir/func.cpp.o"
+  "FileDB.pdb"
+  "FileDB"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FileDB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
